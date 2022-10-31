e26efc2169d8a7324a83c0ae885e4ba98f80df81
David Hasenson <davidhasenson@gmail.com>
2022-10-31 14:46:35 +0100

    mer

--- programs/my_server.py
+++ programs/my_server.py
@@ -1,4 +1,7 @@
+
+
 import socket
+from threading import Thread
 
 
 def return_upper(message):
@@ -19,43 +22,57 @@ def return_multiplied(message, times):
     return message
 
 
+def split_message(data):
+    command = data.split()[0]
+    print(f"command: {command}")
+    message = " ".join(data.split()[1:])
+    print(f"message. {message}")
+    return command, message
+
+
+def modify_message(command, message):
+    ret_data = message
+    for i in command:
+        if i == "u":
+            ret_data = return_upper(ret_data)
+        elif i == "r":
+            ret_data = return_reverse(ret_data)
+        elif i in ["1", "2", "3", "4", "5", "6", "7", "8", "9"]:
+            ret_data = return_multiplied(ret_data, i)
+        else:
+            print("No command given. ")
+    return ret_data
+
+
+def thread_target(conn, addr):
+    # while True:
+    with conn:
+        print('New thread. Connected by', addr)
+        while True:
+            data = conn.recv(1024).decode()
+            if not data:
+                break
+            command, message = split_message(data)
+            print(
+                f"Command received: {command}, Message received: {message} ")
+            ret_data = modify_message(command, message)
+            print(f"ret_data: {ret_data}")
+            conn.sendall(ret_data.encode())
+
+
 def main():
     HOST = '127.0.0.1'
     PORT = 50007
 
     while True:
-
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+            s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
             s.bind((HOST, PORT))
             s.listen(1)
-            conn, addr = s.accept()
-            with conn:
-                print('Connected by', addr)
-                while True:
-
-                    data = conn.recv(1024).decode()
-
-                    if not data:
-                        break
-                    command = data.split()[0]
-                    print(f"command: {command}")
-                    message = " ".join(data.split()[1:])
-                    print(f"message. {message}")
-
-                    print(
-                        f"Command received: {command}, Message received: {message} ")
-                    ret_data = message
-
-                    for i in command:
-                        if i == "u":
-                            ret_data = return_upper(ret_data)
-                        elif i == "r":
-                            ret_data = return_reverse(ret_data)
-                        elif i in ["1", "2", "3", "4", "5", "6", "7", "8", "9"]:
-                            ret_data = return_multiplied(ret_data, i)
-                        else:
-                            print("No command given. ")
-                    conn.sendall(ret_data.encode())
+            while True:
+                conn, addr = s.accept()
+                Thread(target=thread_target, args=(conn, addr), daemon=True).start()
+                print("New thread started")
 
 
 if __name__ == "__main__":
